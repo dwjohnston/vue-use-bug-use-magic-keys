fe1529d39addc2dd88fc9c80b9bfbc1bfe483864
David Johnston <david.johnston@flip.com.au>
2025-11-05 17:14:38 +1100

    Create reproduction

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useMagicKeys } from "@vueuse/core";
+import { watch } from "vue";
+
+const { Shift_X_Y_Z } = useMagicKeys();
+
+function emitEmptyKeyboardEvent() {
+  console.log("x");
+  window.dispatchEvent(new KeyboardEvent("keyup"));
+}
+
+watch(Shift_X_Y_Z, () => {
+  alert("hello world");
+});
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-screen h-screen">
+    <button @click="emitEmptyKeyboardEvent">Click me</button>
+  </div>
+</template>
